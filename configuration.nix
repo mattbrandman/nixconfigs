8e614c8fae496420a831c72a5a6cc30f18c71f2b
matt.brandman@gmail.com <matt.brandman@gmail.com>
2024-09-17 12:21:13 -0400

    updates

--- configuration.nix
+++ configuration.nix
@@ -9,18 +9,19 @@
   # Cache Settings
   # configuration.nix
 
+  # this needs to be active before hyprland TODO: make this automated somehow? https://discourse.nixos.org/t/how-to-set-up-cachix-in-flake-based-nixos-config/31781/3
   nix.settings = {
     substituters = ["https://hyprland.cachix.org"];
     trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
   };
 
-  programs.hyprland = {
-    enable = true;
-    # set the flake package
-    package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
-    # make sure to also set the portal package, so that they are in sync
-    portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
-  };
+  # programs.hyprland = {
+  #   enable = true;
+  #   # set the flake package
+  #   package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
+  #   # make sure to also set the portal package, so that they are in sync
+  #   portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
+  # };
 
   # Enable the Flakes feature and the accompanying new nix command-line tool
   boot.loader.grub.device = "/dev/vda";   # (for BIOS systems only)
