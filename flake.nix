6f639df809ea14a2dffb8491efbe1daa7e2ce1a2
matt.brandman@gmail.com <matt.brandman@gmail.com>
2024-09-17 12:02:19 -0400

    updates

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     nixCats.url = "github:BirdeeHub/nixCats-nvim?dir=nix";
-    inputs.hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
+    hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
     # Include any additional inputs if needed
     # For example:
     # neovim-nightly-overlay = {
@@ -28,7 +28,6 @@
       nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
         inherit system;
         specialArgs = { inherit inputs; };
-
         modules = [
           ./configuration.nix
           nc.nixosModules.default
