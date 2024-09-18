bf12146cdb5243f069b8538134f297b46a886706
matt.brandman@gmail.com <matt.brandman@gmail.com>
2024-09-17 21:24:47 -0400

    updates

--- configuration.nix
+++ configuration.nix
@@ -21,7 +21,6 @@
 
   programs.hyprland = {
     enable = true;
-    xwayland.enable = true;
     # set the flake package
     package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
     # make sure to also set the portal package, so that they are in sync
