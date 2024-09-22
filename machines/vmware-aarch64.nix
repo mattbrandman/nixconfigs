d85a43ce25338717872584360de045affcf8cdd6
matt.brandman@gmail.com <matt.brandman@gmail.com>
2024-09-22 12:59:28 -0400

    updates

---
+++ machines/vmware-aarch64.nix
@@ -0,0 +1,39 @@
+{ config, pkgs, inputs, ... }:
+
+{
+  imports = [
+    # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+  ];
+
+  # Enable the Flakes feature and the accompanying new nix command-line tool
+  boot.loader.grub.device = "/dev/sda";   # (for BIOS systems only)
+  boot.loader.systemd-boot.enable = true; # (for UEFI systems only)
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  programs.hyprland.enable = true;
+  environment.systemPackages = with pkgs; [
+    # Flakes clones its dependencies through the git command,
+    # so git must be installed first
+    git
+    vim
+    wget
+    curl
+    qt6.qtwayland
+    tmux
+    alacritty
+    kitty
+  ];
+  
+  # Set the default editor to vim
+  nixCats.enable = true;
+  environment.variables.EDITOR = "nvim";
+
+  # Virtualisation Settings
+  virtualisation.vmware.guest.enable = true;
+  environment.sessionVariables = {
+    WLR_NO_HARDWARE_CURSORS = "1";
+    WLR_RENDERER_ALLOW_SOFTWARE= "1";
+  };
+
+  # ......
+}
