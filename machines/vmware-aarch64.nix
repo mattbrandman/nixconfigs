8606e9aabf95832488843eedb5f9dfafa63f6fc4
matt.brandman@gmail.com <matt.brandman@gmail.com>
2024-09-23 08:20:47 -0400

    updates

--- machines/vmware-aarch64.nix
+++ machines/vmware-aarch64.nix
@@ -48,7 +48,7 @@
   environment.sessionVariables = {
     WLR_NO_HARDWARE_CURSORS = "1";
     WLR_RENDERER_ALLOW_SOFTWARE= "1";
-    # LIBGL_ALWAYS_SOFTWARE = "1";
+    LIBGL_ALWAYS_SOFTWARE = "1";
   };
   environment.variables = {
     WLR_NO_HARDWARE_CURSORS = "1";
