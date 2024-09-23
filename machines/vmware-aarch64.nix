01fd82ba104534509e86c5a1508239a9d076adbe
matt.brandman@gmail.com <matt.brandman@gmail.com>
2024-09-22 21:30:03 -0400

    updates

--- machines/vmware-aarch64.nix
+++ machines/vmware-aarch64.nix
@@ -55,7 +55,7 @@
     WLR_RENDERER_ALLOW_SOFTWARE= "1";
     LIBGL_ALWAYS_SOFTWARE = "1";
   };
-  programs.hyprland.xwayland.enable = true;
+  # programs.hyprland.xwayland.enable = true;
 
 
   # ......
