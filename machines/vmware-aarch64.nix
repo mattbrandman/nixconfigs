a64f317c593757b3fd870860da3dce52700284a7
matt.brandman@gmail.com <matt.brandman@gmail.com>
2024-09-23 10:53:19 -0400

    updates

--- machines/vmware-aarch64.nix
+++ machines/vmware-aarch64.nix
@@ -6,10 +6,11 @@
     ./hardware-configuration.nix
   ];
   hardware.graphics.enable = true;
+  hardware.opengl.enable = true;
   # Login Manager
-  # services.displayManager.sddm.enable = true; #This line enables sddm
+  services.displayManager.sddm.enable = true; #This line enables sddm
   # services.displayManager.sddm.theme = "where_is_my_sddm_theme";
-  # services.xserver.enable = true; # Might need this for Xwayland  
+  services.xserver.enable = true; # Might need this for Xwayland  
 
   # Enable the Flakes feature and the accompanying new nix command-line tool
   boot.loader.grub.device = "/dev/sda";   # (for BIOS systems only)
@@ -33,10 +34,10 @@
 
   qt.enable = true;
 
-  users.users.alice = {
+  users.users.matt = {
     isNormalUser  = true;
-    home  = "/home/alice";
-    description  = "Alice Foobar";
+    home  = "/home/matt";
+    description  = "Matt";
     password = "dummy";
     extraGroups = [ "wheel" ];
   };
@@ -50,7 +51,7 @@
   environment.sessionVariables = {
     WLR_NO_HARDWARE_CURSORS = "1";
     WLR_RENDERER_ALLOW_SOFTWARE= "1";
-    # LIBGL_ALWAYS_SOFTWARE = "1";
+    LIBGL_ALWAYS_SOFTWARE = "1";
   };
   # programs.hyprland.xwayland.enable = true;
 
