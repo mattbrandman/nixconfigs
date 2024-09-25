552ab51b9284793fdac5309f6fbf389b06866128
matt.brandman@gmail.com <matt.brandman@gmail.com>
2024-09-25 10:03:44 -0400

    updates

--- home.nix
+++ home.nix
@@ -3,8 +3,14 @@
 {
   # Home Manager needs a bit of information about you and the
   # paths it should manage.
-  home.username = "md";
-  home.homeDirectory = "/home/md";
+  home.username = "jdoe";
+  home.homeDirectory = "/home/jdoe";
+
+  # Packages that should be installed to the user profile.
+  home.packages = [
+    pkgs.htop
+    pkgs.fortune
+  ];
 
   # This value determines the Home Manager release that your
   # configuration is compatible with. This helps avoid breakage
@@ -14,8 +20,21 @@
   # You can update Home Manager without changing this value. See
   # the Home Manager release notes for a list of state version
   # changes in each release.
-  home.stateVersion = "24.05";
 
-#   # Let Home Manager install and manage itself.
-#   programs.home-manager.enable = true;
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  programs.emacs = {
+    enable = true;
+    extraPackages = epkgs: [
+      epkgs.nix-mode
+      epkgs.magit
+    ];
+  };
+
+  services.gpg-agent = {
+    enable = true;
+    defaultCacheTtl = 1800;
+    enableSshSupport = true;
+  };
 }
