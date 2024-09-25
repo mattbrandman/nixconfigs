05cd98c78052385a22984363da81803ee499ec5c
matt.brandman@gmail.com <matt.brandman@gmail.com>
2024-09-25 10:04:11 -0400

    updates

--- flake.nix
+++ flake.nix
@@ -37,7 +37,7 @@
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
-            home-manager.users.md = import ./home.nix;
+            home-manager.users.jdoe = import ./home.nix;
 
             # Optionally, use home-manager.extraSpecialArgs to pass
             # arguments to home.nix
