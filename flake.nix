12c2d9df9fc2436c71c98a0ff79ddd3d00f07c7c
matt.brandman@gmail.com <matt.brandman@gmail.com>
2024-09-25 09:58:38 -0400

    updates

--- flake.nix
+++ flake.nix
@@ -15,7 +15,7 @@
     # };
   };
 
-  outputs = { self, nixpkgs, nixCats, ... }@inputs:
+  outputs = { self, nixpkgs, nixCats, home-manager, ... }@inputs:
     let
       system = "aarch64-linux";
       pkgs = import nixpkgs { inherit system; };
