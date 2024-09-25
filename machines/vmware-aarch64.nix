9d3081066ccfcb91fb4964e976841ff0bdbd45c6
matt.brandman@gmail.com <matt.brandman@gmail.com>
2024-09-25 10:44:36 -0400

    updates

--- machines/vmware-aarch64.nix
+++ machines/vmware-aarch64.nix
@@ -59,6 +59,10 @@
     LIBGL_ALWAYS_SOFTWARE = "1";
   };
   stylix.enable = true;
+  stylix.image = pkgs.fetchurl {
+    url = "https://www.pixelstalk.net/wp-content/uploads/2016/05/Epic-Anime-Awesome-Wallpapers.jpg";
+    sha256 = "enQo3wqhgf0FEPHj2coOCvo7DuZv+x5rL/WIo4qPI50=";
+  };
   stylix.cursor.package = pkgs.bibata-cursors;
   stylix.cursor.name = "Bibata-Modern-Ice";
 }
