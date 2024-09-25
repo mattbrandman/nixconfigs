06f9e094139f649cea0317a04eed891af4004379
matt.brandman@gmail.com <matt.brandman@gmail.com>
2024-09-25 13:57:59 -0400

    updates

--- machines/vmware-aarch64.nix
+++ machines/vmware-aarch64.nix
@@ -37,17 +37,9 @@
     # libsForQ-t5.qt5ct
     # adwaitaqt
   ];
-  nixpkgs.overlays =
-  let
-    # Change this to a rev sha to pin
-    moz-rev = "master";
-    moz-url = builtins.fetchTarball { url = "https://github.com/mozilla/nixpkgs-mozilla/archive/${moz-rev}.tar.gz";};
-    nightlyOverlay = (import "${moz-url}/firefox-overlay.nix");
-  in [
-    nightlyOverlay
-  ];
-  programs.firefox.package = pkgs.latest.firefox-nightly-bin;
+  
   programs.firefox.enable = true;
+  programs.firefox.package = pkgs.firefox-wayland;
 
   users.users.matt = {
     isNormalUser  = true;
